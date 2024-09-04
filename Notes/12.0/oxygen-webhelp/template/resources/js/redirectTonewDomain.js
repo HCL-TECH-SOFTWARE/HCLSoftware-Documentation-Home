e1fb2e98d2d07a6ec00539629e64fcf13acd44a1
Attaullah Syed <attaullah.s@hcl.com>
2024-09-04 16:01:12 +0530

    refreshing

--- Notes/12.0/oxygen-webhelp/template/resources/js/redirectTonewDomain.js
+++
@@ -1,7 +0,0 @@
-/*This will redirect from help.hcltechsw.com to help.hcl-software.com */
-
-export function redirectNewdomain() {
-
-return window.location.href = window.location.href.replace("help.hcltechsw.com", "help.hcl-software.com")
-
-}
